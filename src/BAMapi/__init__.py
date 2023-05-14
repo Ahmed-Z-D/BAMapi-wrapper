984a2ddeb5a05bf324435c76d7bbb583dc05213b
Ahmed-Z-D <ahmedchelby1@gmail.com>
2023-05-14 19:29:28 +0100

    Revise the structure of import statements.

--- src/BAMapi/__init__.py
+++ src/BAMapi/__init__.py
@@ -1,4 +1,14 @@
-from BAMapi.api import *
-from BAMapi.constants import *
+from BAMapi.api import (
+    set_api_keys,
+    api_keys,
+    _base_foreign_exchange_rates,
+    cours_BBE,
+    cours_virement,
+    courbe_BDT,
+    resultat_oprts_politique_monetaire,
+    resultats_emissions_BT,
+    resultats_oprts_echange_BT,
+)
+
+from BAMapi.constants import INSTRUMENTS, API
 from BAMapi.exceptions import *
-from BAMapi.utils import *
