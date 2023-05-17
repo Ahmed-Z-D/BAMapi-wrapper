30f62823186d4a3aa431bf20c73e1c3cf866d9fe
Ahmed-Z-D <ahmedchelby1@gmail.com>
2023-05-17 01:24:34 +0100

    A new fixtures to create and persist the data related to config.ini.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,45 @@
 from unittest.mock import patch
 from pathlib import Path
 import json
+from dataclasses import dataclass
+import importlib
 
 import requests
 import pytest
+from faker import Faker
 
-@pytest.fixture(scope = "function")
+from BAMapi.utils import _initiate_config_file
+
+
+@dataclass
+class PsudoConfFileData:
+    """Rand data for config file"""
+
+    marche_adjud_des_BT: str
+    marche_des_changes: str
+    marche_obligataire: str
+
+
+@pytest.fixture(scope="function")
 def mock_requests_get():
     """Requests get method mock object."""
-    with patch.object(requests,'get') as MockResponse:
+    with patch.object(requests, "get") as MockResponse:
         yield MockResponse.return_value
 
 
-@pytest.fixture(scope = "session", name = "psudo_args_base_req")
+@pytest.fixture(scope="session", name="psudo_args_base_req")
 def psudo_args_for_base_bam_api_get_request():
     return "01651320651", "https://invalid_url.BAMAPI", {"pusdo": "psudo"}
 
 
-@pytest.fixture(scope = "session")
+@pytest.fixture(scope="session")
 def sample_data():
     path = Path(__file__).with_name("sample_cours_bbe.json")
     path = new_path = path.parent / "samples" / path.name
-    with open(path, 'r') as f:
-        return json.load(f)@pytest.fixture(scope="function")
+    with open(path, "r") as f:
+        return json.load(f)
+
+
 @pytest.fixture(scope="function")
 def config_file_path(tmp_path):
     """Provied the path of the cofig file."""
