d1c65570a8e8ade46ea7e0dc04ded1a61d4fd62a
Ahmed-Z-D <ahmedchelby1@gmail.com>
2023-05-17 01:25:29 +0100

    improve tests related to set and display API keys

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,12 +1,14 @@
 import configparser
 import os
+import importlib
 
 import pytest
 from faker import Faker
 
-from BAMapi.constants import KEYS
+from BAMapi.utils import _initiate_config_file, _load_api_keys
 from BAMapi.api import (
     set_api_keys,
+    display_api_keys,
     _base_foreign_exchange_rates,
     cours_BBE,
     cours_virement,
@@ -16,19 +18,81 @@ from BAMapi.api import (
     resultats_oprts_echange_BT,
 )
 
-# def test_set_api_keys_first_time(tmp_path, faker):
-#     config_file_path = tmp_path.parent / "config.ini"
 
-#     marche_adjud_des_BT = faker.ean13()
-#     marche_des_changes = faker.ean13()
-#     marche_obligataire = faker.ean13()
+def test_initiate_config_file(config_file_path, monkeypatch):
+    # monkeypatch _FILE_PATH const; new value: config_file_path instead of __file__
+    target = importlib.import_module("BAMapi.utils")
+    monkeypatch.setattr(target, "_FILE_PATH", config_file_path)
 
-#     r = set_api_keys(marche_adjud_des_BT, marche_des_changes, marche_obligataire)
+    _initiate_config_file()
 
-#     assert r is True
-#     assert KEYS["marche_adjud_des_BT"] == marche_adjud_des_BT
-#     assert KEYS["marche_des_changes"] == marche_des_changes
-#     assert KEYS["marche_obligataire"] == marche_obligataire
+    # check if the file has been configurated correctly
+    config = configparser.ConfigParser()
+    config.read(config_file_path)
+
+    assert config["APIkeys"]["marche_adjud_des_BT"] == ""
+    assert config["APIkeys"]["marche_des_changes"] == ""
+    assert config["APIkeys"]["marche_obligataire"] == ""
+
+
+def test_load_api_keys(config_file_path, monkeypatch):
+    target = importlib.import_module("BAMapi.utils")
+    monkeypatch.setattr(target, "_FILE_PATH", config_file_path)
+
+    api_keys = _load_api_keys()
+
+    valid_keys = ["marche_adjud_des_BT", "marche_des_changes", "marche_obligataire"]
+
+    assert set(valid_keys) == set([k for k in api_keys.keys()])
+    assert set(["", "", ""]) == set([v for v in api_keys.values()])
+
+
+def test_display_api_keys(config_file_path, capsys, monkeypatch, psudo_conf_file):
+    target_path_utils = importlib.import_module("BAMapi.utils")
+    monkeypatch.setattr(target_path_utils, "_FILE_PATH", config_file_path)
+
+    target_path_api = importlib.import_module("BAMapi.api")
+    monkeypatch.setattr(target_path_api, "_FILE_PATH", config_file_path)
+
+    r = set_api_keys(
+        psudo_conf_file.marche_adjud_des_BT,
+        psudo_conf_file.marche_des_changes,
+        psudo_conf_file.marche_obligataire,
+    )
+
+    target_file_consts = importlib.import_module("BAMapi.constants")
+    monkeypatch.setattr(target_file_consts, "KEYS", _load_api_keys())
+    from BAMapi.constants import KEYS
+
+    display_api_keys()
+    output = capsys.readouterr().out.rstrip()
+
+    assert output == str(KEYS)
+
+
+def test_set_api_keys_first_time(config_file_path, psudo_conf_file, monkeypatch):
+    target_path_utils = importlib.import_module("BAMapi.utils")
+    monkeypatch.setattr(target_path_utils, "_FILE_PATH", config_file_path)
+
+    target_path_api = importlib.import_module("BAMapi.api")
+    monkeypatch.setattr(target_path_api, "_FILE_PATH", config_file_path)
+
+    _initiate_config_file()
+
+    r = set_api_keys(
+        psudo_conf_file.marche_adjud_des_BT,
+        psudo_conf_file.marche_des_changes,
+        psudo_conf_file.marche_obligataire,
+    )
+
+    target_file_consts = importlib.import_module("BAMapi.constants")
+    monkeypatch.setattr(target_file_consts, "KEYS", _load_api_keys())
+    from BAMapi.constants import KEYS
+
+    assert r is True
+    assert KEYS["marche_adjud_des_BT"] == psudo_conf_file.marche_adjud_des_BT
+    assert KEYS["marche_des_changes"] == psudo_conf_file.marche_des_changes
+    assert KEYS["marche_obligataire"] == psudo_conf_file.marche_obligataire
 
 
 def test_base_foreign_exchange_rates(faker, mock_requests_get, sample_data):
@@ -36,7 +100,6 @@ def test_base_foreign_exchange_rates(faker, mock_requests_get, sample_data):
     service_api_key = faker.ean13()
     currency_label = faker.currency_code()
     date_time = faker.date()
-
 
     mock_requests_get.json.return_value = sample_data
 
@@ -46,11 +109,10 @@ def test_base_foreign_exchange_rates(faker, mock_requests_get, sample_data):
     assert response == sample_data
 
 
-@pytest.mark.parametrize("currency_label, date_time",
-    [("", ""),
-    (Faker().currency_code(), Faker().date())])
+@pytest.mark.parametrize(
+    "currency_label, date_time", [("", ""), (Faker().currency_code(), Faker().date())]
+)
 def test_cours_BBE(currency_label, date_time, mock_requests_get, sample_data):
-
     mock_requests_get.json.return_value = sample_data
 
     response = cours_BBE(currency_label, date_time)
@@ -59,11 +121,10 @@ def test_cours_BBE(currency_label, date_time, mock_requests_get, sample_data):
     assert response == sample_data
 
 
-@pytest.mark.parametrize("currency_label, date_time",
-    [("", ""),
-    (Faker().currency_code(), Faker().date())])
+@pytest.mark.parametrize(
+    "currency_label, date_time", [("", ""), (Faker().currency_code(), Faker().date())]
+)
 def test_cours_virement(currency_label, date_time, mock_requests_get, sample_data):
-
     mock_requests_get.json.return_value = sample_data
 
     response = cours_virement(currency_label, date_time)
@@ -74,7 +135,6 @@ def test_cours_virement(currency_label, date_time, mock_requests_get, sample_dat
 
 @pytest.mark.parametrize("date", ["", Faker().date()])
 def test_courbe_BDT(date, mock_requests_get, sample_data):
-
     mock_requests_get.json.return_value = sample_data
 
     response = courbe_BDT(date)
@@ -90,7 +150,9 @@ def test_resultat_oprts_politique_monetaire(faker, mock_requests_get, sample_dat
 
     mock_requests_get.json.return_value = sample_data
 
-    response = resultat_oprts_politique_monetaire(date_adjudication_du, date_adjudication_au, instrument)
+    response = resultat_oprts_politique_monetaire(
+        date_adjudication_du, date_adjudication_au, instrument
+    )
 
     assert all(isinstance(d, dict) for d in response)
     assert response == sample_data
@@ -105,6 +167,7 @@ def test_resultats_emissions_BT(faker, mock_requests_get, sample_data):
 
     assert all(isinstance(d, dict) for d in response)
     assert response == sample_data
+
 
 def test_resultats_oprts_echange_BT(faker, mock_requests_get, sample_data):
     date = faker.date()
